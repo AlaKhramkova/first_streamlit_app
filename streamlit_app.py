de139f73efde06a0dc33ec5c5aa0531582ddc70a
AlaKhramkova <alla.khramkova@azati.com>
2023-03-03 21:12:51 +0300

    add new fruit to db

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,4 +35,6 @@ streamlit.header("The Fruit Load List contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
-streamlit.write('Thanks for adding', add_my_fruit)
+with my_cnx.cursor() as my_cursor:
+    streamlit.write('Thanks for adding', add_my_fruit)
+    my_cursor.execute("insert into fruit_load_list values ('" + add_my_fruit + "')")
